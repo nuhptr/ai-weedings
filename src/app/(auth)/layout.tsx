8ea28efd0dc8269f7033dcbb7232ad2c0f649490
nuhptr <nugrahaadi733@gmail.com>
2025-02-27 12:03:37 +0700

    refactor: :art: improve layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
 export default function AuthLayout({ children }: childrenProps) {
-    return <div>{children}</div>
+    return (
+        <section className="relative w-dvw h-dvh flex items-center justify-center">
+            <div className="w-full max-w-2xl p-10">{children}</div>
+        </section>
+    )
 }
